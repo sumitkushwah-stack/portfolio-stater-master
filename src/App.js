827cb8a70f455f49e2df6a053105396762fa1a17
sumit kushwah <sk17595221@gmail.com>
2022-05-30 00:09:31 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import Navbar from "./components/Navbar/navbar";
+import './App.css'
 
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
